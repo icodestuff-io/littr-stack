5836b92f53e5b9570d05acdba178d0fdab8124dd
santoine@nerdery.com <antoinesolomon5@gmail.com>
2022-01-23 16:22:46 -0600

    init commit.

---
+++ resources/js/Pages/Foo.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+const Foo: FunctionComponent = () => {
+    return (
+        <div className="h-screen bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 flex items-center justify-center">
+            <h1 className="text-white text-center text-6xl font-extrabold tracking-tight">LITTR Stack Works!</h1>
+        </div>
+    )
+}
+
+export default Foo;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.tsx",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
